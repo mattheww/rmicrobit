7283485bbf7ec979c9f6920754d3157e927cd070
Matthew Woodcraft <matthew@woodcraft.me.uk>
2019-04-21 18:38:13 +0100

    prelude: add graphics::scrolling::Animate

--- src/prelude.rs
+++ src/prelude.rs
@@ -15,10 +15,12 @@
 //! [`Frame`]               | `frame.set()` |
 //! [`MicrobitGpioExt`]     | `GPIO.split_by_kind()` |
 //! [`PollButton`]          | `button_a.poll_event()` |
+//! [`Animate`]             | `scrolling_images.tick()` |
 //!
 //! [`MicrobitGpioExt`]: crate::gpio::MicrobitGpioExt
 //! [`PollButton`]: crate::buttons::core::PollButton
 //! [`Frame`]: tiny_led_matrix::Frame
+//! [`Animate`]: crate::graphics::scrolling::Animate
 
 // I'm hiding these from rustdoc to prevent it choosing some of them as the
 // main page for the traits (eg for Frame). It seems least misleading to hide
@@ -32,3 +34,6 @@ pub use crate::gpio::MicrobitGpioExt as _mb_gpio_microbit_gpio_ext;
 
 #[doc(hidden)]
 pub use crate::buttons::core::PollButton as _mb_buttons_core_poll_button;
+
+#[doc(hidden)]
+pub use crate::graphics::scrolling::Animate as _mb_graphics_scrolling_animate;
